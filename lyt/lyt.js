685657ddef2fe3b466994956c8b7d36a828054aa
Liu20040607 <81454092+Liu20040607@users.noreply.github.com>
2024-06-28 16:06:08 +0800

    version1

---
+++ lyt/lyt.js
@@ -0,0 +1,32 @@
+function showTime(clock) {
+    var s1='2024-06-20';
+    s1 = new Date(s1.replace(/-/g,"/"));
+    var now = new Date();
+    var dateDiff = now.getTime() - s1.getTime();
+    var dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000));
+    var leave1=dateDiff%(24*3600*1000);   
+    var hours=Math.floor(leave1/(3600*1000));
+
+    var leave2=leave1%(3600*1000)    
+    var minutes=Math.floor(leave2/(60*1000));
+
+    var leave3=leave2%(60*1000);      
+    var seconds=Math.round(leave3/1000);
+
+    var time =  dayDiff+"天 "+hours+"小時 "+minutes+" 分鐘"+seconds+" 秒";
+    clock.innerHTML = "我們已經度過了: " + time;
+}
+window.onload = function () {
+    var clock = document.getElementById("clock");
+    window.setInterval(function () {
+        showTime(clock);
+    }, 1000);
+}
+function clock(){
+    alert('特別數字就請你對劉羿承說"我愛你"才告訴你');
+}
+
+function s() {
+    　var Today=new Date();
+    　alert("今天日期是 " + Today.getFullYear()+ " 年 " + (Today.getMonth()+1) + " 月 " + Today.getDate() + " 日"+'又是愛你的一天喔!寶寶~');
+    }
